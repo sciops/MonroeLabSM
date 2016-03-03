dd725deb01c33bc91d3c7016657d29ce38f6a772
stephen williams <stephen.ray.williams@gmail.com>
2016-03-02 21:25:17 -0600

    Initial commit. Trying out spark framework with aws beanstalk.

---
+++ src/main/java/com/mycompany/monroelabsm/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.monroelabsm;
+
+/**
+ *
+ * @author Stephen R. Williams
+ * 
+ * Started with git clone of SparkTomcat by Leonan Luppi
+ * https://github.com/leonanluppi/SparkTomcat
+ * https://github.com/leonanluppi/SparkTomcat/blob/master/src/main/java/com/sparkjava/tomcat/Main.java
+ */
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
+
+public class Main implements SparkApplication {
+    //must move all verbs to init method, out of main
+    public void init() {
+        get("/hello", (req, res) -> "Hello World. This is the /hello response in MonroeLabSM");
+        get("/hello123", (req, res) -> "Hello World, test 123. This is the /hello123 response in MonroeLabSM");
+    }   
+    
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.init();
+    }
+}
