2b3b6955e603f03aa72ac77555fabdd1d3b31ca5
stephen williams <stephen.ray.williams@gmail.com>
2016-03-09 05:44:58 -0600

    Got some kind of front-end responding back now. Problem is that root request returns what the browser interprets as a string instead of html rendered. Replaced root get method to 301 redirect, but that got the same result.

--- src/main/java/com/mycompany/monroelabsm/Main.java
+++ src/main/java/com/mycompany/monroelabsm/Main.java
@@ -25,7 +25,17 @@ public class Main implements SparkApplication {
         staticFileLocation("/public"); // Static files. 
         
         BoxKeyController bkc = new BoxKeyController(new BoxKeyService());
+        
+        //silly root response removed, see replacement below
         //get("/", (req, res) -> "<html>Hello World. This is the / (root) response in MonroeLabSM <br><br><img src=\"http://img.memecdn.com/square-root_o_895898.jpg\" /></html>");        
+        
+        //new root response
+        //when deployed on aws, get request on root will return index.html, but not interpreted by browser as a string instead of html. trying redirect instead
+        get("/", (req, res) -> {
+            res.redirect("/index.html", 301); //HTTP code 301: moved permanently
+            return "Root response";//have to return something? this shouldn't be seen by user?
+        });
+        
         get("/hello", (req, res) -> "Hello World. This is the /hello response in MonroeLabSM");
         //get("/hello123", (req, res) -> "Hello World, test 123. This is the /hello123 response in MonroeLabSM");
         
