8d3fde6c6133fe8e8ea2c19cd9b0410d11af3590
stephen williams <stephen.ray.williams@gmail.com>
2016-03-09 05:34:57 -0600

    fix for static files was something silly like forgetting to re-build. removed some un-needed images and the root and extra hello world endpoint.

--- src/main/java/com/mycompany/monroelabsm/Main.java
+++ src/main/java/com/mycompany/monroelabsm/Main.java
@@ -14,6 +14,7 @@ package com.mycompany.monroelabsm;
  * https://github.com/leonanluppi/SparkTomcat/blob/master/src/main/java/com/sparkjava/tomcat/Main.java
  */
 import static spark.Spark.*;
+import static spark.Spark.staticFileLocation;
 import spark.servlet.SparkApplication;
 
 public class Main implements SparkApplication {
@@ -21,13 +22,12 @@ public class Main implements SparkApplication {
     @Override
     public void init() {
         //http://sparkjava.com/documentation.html#static-files
-        //TODO: 404ing. wrong path?
         staticFileLocation("/public"); // Static files. 
         
         BoxKeyController bkc = new BoxKeyController(new BoxKeyService());
-        get("/", (req, res) -> "<html>Hello World. This is the / (root) response in MonroeLabSM <br><br><img src=\"http://img.memecdn.com/square-root_o_895898.jpg\" /></html>");        
+        //get("/", (req, res) -> "<html>Hello World. This is the / (root) response in MonroeLabSM <br><br><img src=\"http://img.memecdn.com/square-root_o_895898.jpg\" /></html>");        
         get("/hello", (req, res) -> "Hello World. This is the /hello response in MonroeLabSM");
-        get("/hello123", (req, res) -> "Hello World, test 123. This is the /hello123 response in MonroeLabSM");
+        //get("/hello123", (req, res) -> "Hello World, test 123. This is the /hello123 response in MonroeLabSM");
         
     }   
     
