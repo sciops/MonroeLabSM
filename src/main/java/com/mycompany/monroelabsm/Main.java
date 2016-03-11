770a768a3a2543f51176b2404b5d2629f0143b84
stephen williams <stephen.ray.williams@gmail.com>
2016-03-11 01:30:15 -0600

    Tried more things. Still content type text/plain. Can't seem to find an example of spark on a tomcat server that serves 	static files?

--- src/main/java/com/mycompany/monroelabsm/Main.java
+++ src/main/java/com/mycompany/monroelabsm/Main.java
@@ -8,7 +8,7 @@ package com.mycompany.monroelabsm;
 /**
  *
  * @author Stephen R. Williams
- * 
+ *
  * Started with git clone of SparkTomcat by Leonan Luppi
  * https://github.com/leonanluppi/SparkTomcat
  * https://github.com/leonanluppi/SparkTomcat/blob/master/src/main/java/com/sparkjava/tomcat/Main.java
@@ -18,21 +18,28 @@ import static spark.Spark.staticFileLocation;
 import spark.servlet.SparkApplication;
 
 public class Main implements SparkApplication {
+
     //must move all verbs to init method, out of main
     @Override
     public void init() {
         //http://sparkjava.com/documentation.html#static-files
         staticFileLocation("/public"); // Static files. 
-        
+
         BoxKeyController bkc = new BoxKeyController(new BoxKeyService());
-        
-        //get("/", (req, res) -> "<html>Hello World. This is the / (root) response in MonroeLabSM <br><br><img src=\"http://img.memecdn.com/square-root_o_895898.jpg\" /></html>");        
+
+        //get("/", (req, res) -> "<html>Hello World. This is the / (root) response in MonroeLabSM <br><br><img src=\"http://img.memecdn.com/square-root_o_895898.jpg\" /></html>");  
+        get("/", (req, res) -> {
+            //res.header("Content-Type","text/html");
+            res.type("text/html");
+            //res.redirect("/index.html", 301); // moved permanently
+            return "This is a required string for the root method return statement";
+        });
 
         get("/hello", (req, res) -> "Hello World. This is the /hello response in MonroeLabSM");
         //get("/hello123", (req, res) -> "Hello World, test 123. This is the /hello123 response in MonroeLabSM");
-        
-    }   
-    
+
+    }
+
     public static void main(String[] args) {
         Main m = new Main();
         m.init();
